cddd0f23a0f297217ba8dadd0a8ae4dc7c0dae33
Saisrikar <saisrikar1220@gmail.com>
2026-08-30 20:49:38 +0530

    ✅ Recursion - Recursive Sum of Array Elements | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-30

---
+++ hackerrank/medium/recursion-recursive-sum-of-array-elements/solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Solution {
+
+    static int sumArray(int[] arr, int index) {
+        if (index == arr.length) {
+            return 0;
+        }
+
+        return arr[index] + sumArray(arr, index + 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(sumArray(arr, 0));
+    }
+}
