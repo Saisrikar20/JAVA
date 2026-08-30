ac3fd6af3f01e1c206dfdba5c212af5bf3ac2159
Saisrikar <saisrikar1220@gmail.com>
2026-08-30 20:34:18 +0530

    ✅ Recursion - Counting Active Bits in a Device Monitoring System | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-30

---
+++ hackerrank/medium/recursion-counting-active-bits-in-a-device-monitoring-system/solution.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    static int countSetBits(int number) {
+        if (number == 0) {
+            return 0;
+        }
+
+        return (number % 2) + countSetBits(number / 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+
+        System.out.println(countSetBits(number));
+    }
+}
