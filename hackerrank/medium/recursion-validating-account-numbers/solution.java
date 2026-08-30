f6dfb12aae2039ca92697cde561594e8dacc7beb
Saisrikar <saisrikar1220@gmail.com>
2026-08-30 20:56:57 +0530

    ✅ Recursion - Validating Account Numbers | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-30

---
+++ hackerrank/medium/recursion-validating-account-numbers/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+
+    static int countDigits(int n) {
+        if (n < 10) {
+            return 1;
+        }
+
+        return 1 + countDigits(n / 10);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(countDigits(n));
+    }
+}
